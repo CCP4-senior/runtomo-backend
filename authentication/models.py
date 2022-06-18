11ad344fcdd7fef4d63cc082d894a2ee79cd1088
yadonoya <mscottschreiber@gmail.com>
2022-06-18 10:35:53 +0900

    fix: change create_super -> create_superuser

--- authentication/models.py
+++ authentication/models.py
@@ -22,7 +22,7 @@ class CustomUserManager(BaseUserManager):
         return new_user
 
 
-    def create_super(self, email, password, **extra_fields):
+    def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_active', True)
