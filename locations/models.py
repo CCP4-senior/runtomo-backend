feb7a326dfb09d9ff7b0ac8659190ac4dc8aa2e9
AlexBrown4020 <balex3497@gmail.com>
2022-06-21 21:45:00 +0900

    Feat: Implement LocationType app

--- locations/models.py
+++ locations/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Location(models.Model):
-    type = models.IntegerField()
+    # type = models.ForeignKey(LocationType, on_delete=models.CASCADE)
     long = models.DecimalField(read_only=True, max_digits=19, decimal_places=16)
     lat = models.DecimalField(read_only=True, max_digits=18, decimal_places=16)
