2e8153a5d8f499ca1c502710af82d1737d75498d
AlexBrown4020 <balex3497@gmail.com>
2022-06-22 14:36:07 +0900

    Fix: Add sessionAuthentication to prevent localhost error

--- sole_mates/settings.py
+++ sole_mates/settings.py
@@ -133,6 +133,7 @@ REST_FRAMEWORK={
     'NON_FIELD_ERRORS_KEY': 'Errors',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
         ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 3
--- sole_mates/urls.py
+++ sole_mates/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
    path('swagger<format>.json|.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'), 
-   path('locations', include('locations.urls')),
+   path('locations/', include('locations.urls')),
 ]
 
