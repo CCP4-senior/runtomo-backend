13aa41beaf678c8447720f225972335a56ba711b
AlexBrown4020 <balex3497@gmail.com>
2022-06-22 11:57:24 +0900

    Fix: Remove read_only causing error in Location

--- locations/models.py
+++ locations/models.py
@@ -3,5 +3,5 @@ from location_types.models import LocationType
 
 class Location(models.Model):
     type = models.ForeignKey(LocationType, on_delete=models.CASCADE)
-    long = models.DecimalField(read_only=True, max_digits=19, decimal_places=16)
-    lat = models.DecimalField(read_only=True, max_digits=18, decimal_places=16)
+    long = models.DecimalField( max_digits=19, decimal_places=16)
+    lat = models.DecimalField(max_digits=18, decimal_places=16)
