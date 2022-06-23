14fb8dd2ac3a93331bc8690cf229dae16072adf4
AlexBrown4020 <balex3497@gmail.com>
2022-06-23 19:47:49 +0900

    Fix:Update admin list display for date, time, running-duration

--- events/admin.py
+++ events/admin.py
@@ -5,5 +5,5 @@ from .models import Event
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
-    list_display = ['title', 'location', 'created_at', 'ward']
+    list_display = ['title', 'location', 'created_at', 'ward', 'date', 'time', 'running_duration']
     list_filter = ['location', 'created_at']
