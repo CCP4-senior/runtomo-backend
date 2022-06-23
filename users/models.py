bdae4ce879704a8f23a174666d8dc94addf815db
yadonoya <mscottschreiber@gmail.com>
2022-06-23 20:50:38 +0900

    fix: minor bug fix for auth, users table

--- users/models.py
+++ users/models.py
@@ -1,13 +1,20 @@
 from django.contrib.postgres.fields import ArrayField
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.forms import CharField
 
 User=get_user_model()
+
+test_types = (
+    ('AVID', 'Avid'),
+    ('COMPETITIVE', 'Competitive'),
+    ('FAST', 'Fast')
+)
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     # runner_type = models.ForeignKey('RunnerType', on_delete=models.CASCADE)
-    runner_type = ArrayField(models.CharField(max_length=200), blank=True)
+    runner_type = models.CharField(max_length=200, choices=test_types)
     # age, gender....
 
     def __str__(self):
