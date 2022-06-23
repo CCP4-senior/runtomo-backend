5c41e1d468356fb651cac67eb1f0ebde512ad54e
AlexBrown4020 <balex3497@gmail.com>
2022-06-23 17:13:32 +0900

    Feat:Add date, time and running duration fields to events

--- events/models.py
+++ events/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from wards.models import Ward
+from datetime import datetime
 
 # Create your models here.
 
@@ -8,11 +9,11 @@ User=get_user_model()
 
 class Event(models.Model):
 
-
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add = True)
     ward = models.ForeignKey(Ward, on_delete=models.CASCADE, null=True)
-
-
+    date = models.DateField(default=datetime.now())
+    time = models.TimeField(default=datetime.now())
+    running_duration = models.IntegerField(default=0)
