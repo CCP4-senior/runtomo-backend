12382e070bf24797f11d0217eeea2c3676556393
AlexBrown4020 <balex3497@gmail.com>
2022-06-23 19:14:04 +0900

    Fix: remove session authentication from framework

--- sole_mates/settings.py
+++ sole_mates/settings.py
@@ -135,7 +135,6 @@ REST_FRAMEWORK={
     'NON_FIELD_ERRORS_KEY': 'Errors',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
         ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 3
