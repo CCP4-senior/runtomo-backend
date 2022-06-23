03273860fef504c3301431a738f2eaf3989fc963
mayl730 <maylaixyz@gmail.com>
2022-06-23 20:57:04 +0900

    wip: put method for profile

--- users/models.py
+++ users/models.py
@@ -1,15 +1,22 @@
+from django.forms import CharField
 from django.contrib.postgres.fields import ArrayField
 from django.db import models
 from django.contrib.auth import get_user_model
 
 User=get_user_model()
 
+gender = (
+    ('F', 'Female'),
+    ('M', 'Male'),
+    ('OTHER', 'Other'),
+)
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # runner_type = models.ForeignKey('RunnerType', on_delete=models.CASCADE)
     runner_type = ArrayField(models.CharField(max_length=200), blank=True)
-    age = models.IntegerField()
-    # age, gender....
+    age = models.IntegerField(default=0)
+    # gender = models.CharField(max_length=10, choices=gender)
+    # runner_type = models.ForeignKey('RunnerType', on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.user.username} Profile'
