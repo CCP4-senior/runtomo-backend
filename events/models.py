2289bfc6327644417ff8010b715df36bab2505a6
Yuka <72680138+ys-6-1@users.noreply.github.com>
2022-06-24 13:07:35 +0900

    Revert "Address timezone migration issue"

--- events/models.py
+++ events/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from wards.models import Ward
 from datetime import datetime
-from django.utils import timezone
 
 # Create your models here.
 
@@ -15,8 +14,8 @@ class Event(models.Model):
     location = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add = True)
     ward = models.ForeignKey(Ward, on_delete=models.CASCADE, null=True)
-    date = models.DateTimeField(default=timezone.now())
-    time = models.DateTimeField(default=timezone.now())
+    date = models.DateTimeField(default=datetime.now())
+    time = models.DateTimeField(default=datetime.now())
     running_duration = models.IntegerField(default=0)
     description = models.TextField(max_length=255)
     image = models.CharField(max_length=300, null=True, blank=True)
