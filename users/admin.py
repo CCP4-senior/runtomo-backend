5f018bce107f81bbe47c6c99c0c38fcb34f14831
mayl730 <maylaixyz@gmail.com>
2022-06-25 11:35:01 +0900

    fix: remove runnertype field for admin panel

--- users/admin.py
+++ users/admin.py
@@ -3,14 +3,3 @@ from .models import Profile, RunnerType
 # Register your models here.
 
 admin.site.register(Profile)
-admin.site.register(RunnerType)
-
-# from django.contrib import admin
-# from .models import Event
-
-# # Register your models here.
-
-# @admin.register(Event)
-# class EventAdmin(admin.ModelAdmin):
-#     list_display = ['title', 'location', 'created_at']
-#     list_filter = ['location', 'created_at']
