06d16bcdc53cf7a2fc8d5da2a9e96ec83256be5b
AlexBrown4020 <balex3497@gmail.com>
2022-06-27 11:19:31 +0900

    Feat:Modify EventUser model, add functions to add/remove attendance.

--- event_users/models.py
+++ event_users/models.py
@@ -7,5 +7,12 @@ User=get_user_model()
 
 class EventUser(models.Model):
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
-    user = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='user')
-    attending = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def add_user_attendance(self, user):
+        registration = EventUser.objects.create(user = user, event = self)
+        registration.save()
+
+    def remove_user_attendance(self, user):
+        registration = EventUser.objects.get(user = user, event = self)
+        registration.delete()
