088d9eeb5a6b9c600e4c7b0b8988ec30b08e2926
yadonoya <mscottschreiber@gmail.com>
2022-06-30 13:08:46 +0900

    fix: updent events.serializers to use nested creator field

--- events/serializers.py
+++ events/serializers.py
@@ -5,21 +5,26 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+class creatorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+
 class EventCreationSerializer(serializers.ModelSerializer):
 
     title = serializers.CharField(max_length=255)
-    creator = serializers.SlugRelatedField(queryset=User.objects.all(), slug_field='email')
     location = serializers.CharField(max_length=255)
     ward = serializers.SlugRelatedField(queryset = Ward.objects.all(),slug_field = 'ward_name')
 
     class Meta:
         model=Event
         fields = ['id', 'creator', 'title', 'location', 'created_at', 'ward', 'date', 'time', 'running_duration', 'description', 'image', 'lat', 'long']
+        depth = 1
 
 class EventDetailSerializer(serializers.ModelSerializer):
 
     title = serializers.CharField(max_length=255)
-    creator = serializers.SlugRelatedField(queryset=User.objects.all(), slug_field='email')
+    creator = creatorSerializer(read_only=True)
     location = serializers.CharField(max_length=255)
     created_at = serializers.DateTimeField('date created')
     ward = serializers.CharField(source='ward.ward_name', default=None)
@@ -27,3 +32,4 @@ class EventDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model=Event
         fields = ['id', 'creator', 'title', 'location', 'ward', 'created_at', 'date', 'time', 'running_duration', 'description', 'image', 'lat', 'long'] 
+        depth = 1
