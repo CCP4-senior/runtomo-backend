6fea7790b0567449eb2800ee6415185c01863962
mayl730 <maylaixyz@gmail.com>
2022-07-05 15:00:47 +0900

    feat: implement get all comments from 1 event

--- event_comments/serializers.py
+++ event_comments/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 from .models import EventComments
+from django.contrib.auth import get_user_model
+User=get_user_model()
+
+class CommentUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
 
 class EventCommentsSerializer(serializers.ModelSerializer):
-    comment_user = serializers.StringRelatedField(read_only=True)
+    comment_user = CommentUserSerializer(read_only=True)
     class Meta:
         model = EventComments
         exclude = ('event',)
         # fields = "__all__"
+        depth = 1
