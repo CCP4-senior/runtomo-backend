b78a73af5440a2352ab5b6db607979ab48095be2
mayl730 <maylaixyz@gmail.com>
2022-07-05 13:35:45 +0900

    add: event comments serializer

---
+++ event_comments/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import EventComments
+
+class EventCommentsSerializer(serializers.ModelSerializer):
+    comment_user = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = EventComments
+        exclude = ('event',)
+        # fields = "__all__"
