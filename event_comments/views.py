8701b6147c5d71210e81452a919b27e83feae2e0
mayl730 <maylaixyz@gmail.com>
2022-07-05 13:45:28 +0900

    wip: event comment create view

--- event_comments/views.py
+++ event_comments/views.py
@@ -1,3 +1,30 @@
+from multiprocessing import Event
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+from event_comments.models import EventComments
+from . import serializers
+from events.models import Event
 
 # Create your views here.
+
+class EventCommentCreate(generics.CreateAPIView):
+    serializer_class = serializers.EventCommentsSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return EventComments.objects.all()
+
+    def perform_create(self, serializer):
+        pk = self.kwargs.get('pk')
+        event = Event.objects.get(pk=pk)
+
+        event_user = self.request.user
+        event_comments_queryset =EventComments.objects.filter(
+            event=event, event_user=event_user)
+
+        event.save()
+
+        serializer.save(event=event, event_user=event_user)
