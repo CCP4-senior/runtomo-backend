7061a584c0e65ae5ecf899ab8124be87b065df32
mayl730 <maylaixyz@gmail.com>
2022-07-06 21:08:07 +0900

    migrate: add description field  to profile

--- users/models.py
+++ users/models.py
@@ -14,6 +14,7 @@ class Profile(models.Model):
     run_frequency = models.CharField(max_length=255, null=True)
     estimated10k = models.CharField(max_length=255, null=True)
     estimated5k = models.CharField(max_length=255, null=True)
+    description = models.CharField(max_length=500, null=True)
 
     def __str__(self):
         return f'{self.user} Profile'
